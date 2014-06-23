fb8f74ac46be1650bd9c176d9c22429ed7dc5d0d
aaron addleman <aaronaddleman@gmail.com>
2014-06-23 09:33:40 -0700

    fix error with setting ssh banner

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -6,13 +6,14 @@ class { 'pam_mgmt': }
 
 file { ["/etc/motd", "/etc/issue"]:
   ensure => file,
-  content => "ALERT! You are entering into a secured area! Your IP, Login Time, Username has been noted and has been sent to the server administrator! This service is restricted to authorized users only. All activities on this system are logged. Unauthorized access will be fully investigated and reported to the appropriate law enforcement agencies."
+  content => "ALERT! You are entering into a secured area! Your IP, Login Time, Username has been noted and has been sent to the server administrator! This service is restricted to authorized users only. All activities on this system are logged. Unauthorized access will be fully investigated and reported to the appropriate law enforcement agencies.
+  "
 }
 
 # set banner for sshd
 augeas { "/etc/ssh/sshd_config":
-  context => "${title}",
-  changes => [ "set banner 'ALERT! You are entering into a secured area! Your IP, Login Time, Username has been noted and has been sent to the server administrator! This service is restricted to authorized users only. All activities on this system are logged. Unauthorized access will be fully investigated and reported to the appropriate law enforcement agencies.'"
+  context => "/files/etc/sshd_config",
+  changes => [ "set Banner 'ALERT! You are entering into a secured area! Your IP, Login Time, Username has been noted and has been sent to the server administrator! This service is restricted to authorized users only. All activities on this system are logged. Unauthorized access will be fully investigated and reported to the appropriate law enforcement agencies.'"
   ],
   notify => Service["sshd"],
 }
